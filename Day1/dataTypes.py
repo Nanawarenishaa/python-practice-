471c0022d1c0dfdbcc022af3b294c51c0732e0ce
Nanawarenishaa <164838299+Nanawarenishaa@users.noreply.github.com>
2025-02-15 20:14:03 +0530

    Add files via upload

--- Day1/dataTypes.py
+++ Day1/dataTypes.py
@@ -1,9 +1,13 @@
 age = 20
 name = "nisha"
+height = 5.4
 is_student = True
 
 print("age:",age)
-
+# print(type(age))
 print("age:",name)
-
-print("age:",True)
+# print(type(name))
+print("is_student:",True)
+# print(type(is_student))
+print("Height:",height)
+# print(type(height))
