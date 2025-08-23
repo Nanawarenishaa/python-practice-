1841764c15202e77b1a6c2885dcd92edaab45ee1
Nanawarenishaa <164838299+Nanawarenishaa@users.noreply.github.com>
2025-08-23 06:49:25 -0700

    Add files via upload

---
+++ basicsPy/program2.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print( " first num: ", num1)
+print(" second num: ", num2)
+
+print(" sum of two numbers: ", num1 + num2 )
+sum = num1 + num2
+print(sum)
---
+++ basicsPy/program4.py
@@ -0,0 +1,8 @@
+a = input(" Enter string: ")
+b = input(" Enter string: ")
+
+print(" string after concatenate : " , a + " " + b )
+
+name = input(" Enter your name: ")
+
+print(" Hello ", name)
