c996dd21d75021261f6e85b1046c02abacfdf69b
Nanawarenishaa <164838299+Nanawarenishaa@users.noreply.github.com>
2025-08-27 10:00:53 -0700

    Add files via upload

---
+++ basicsPy/string.py
@@ -0,0 +1,17 @@
+name="Nisha"
+message='hellooo everyoneee!'
+multiline1 = """This is a multiline String"""
+multiline2 = '''this is again a multiline string'''
+
+print(name)
+print(message)
+print(multiline1)
+print(multiline2)
+
+for i in range(-1,-6,-1):
+  print(name[i], end=" ")
+
+print(end="\n")
+
+for i in range(0,5,1):
+  print(name[i], end=" ")
