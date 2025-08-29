caf46a187f34d927f647e23b0151e03d206d7dbb
Nanawarenishaa <164838299+Nanawarenishaa@users.noreply.github.com>
2025-08-29 10:34:44 -0700

    Add files via upload

---
+++ basicsPy/set.py
@@ -0,0 +1,13 @@
+num = {1,2,2,3,3,4,5}
+print(num)  
+"""No duplicates allowed"""
+numbers = {1, 2, 3, 4, 2, 3}
+print("Set:", numbers)  
+
+
+numbers.add(5)
+print("After adding:", numbers)
+
+
+numbers.remove(3)
+print("After removing 3:", numbers)
