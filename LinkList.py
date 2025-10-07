d1b85d5ffa117ac2c1d13d8cc7d23131cffb4db3
Nanawarenishaa <164838299+Nanawarenishaa@users.noreply.github.com>
2025-10-07 09:49:25 -0700

    Add files via upload

---
+++ LinkList.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+
+# 🔹 Using Linked List
+ll = LinkedList()
+
+num=int(input("Enter value:"))
+while num > 0:
+    rem=num%10
+    ll.insert(rem)
+    num//=10
+
+ll.display()
