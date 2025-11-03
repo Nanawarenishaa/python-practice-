fed5233bb72cff2af46af0d15ed51486d645634b
Nanawarenishaa <164838299+Nanawarenishaa@users.noreply.github.com>
2025-11-03 03:33:02 -0800

    Add files via upload

--- basicsPy/LinkList.py
+++ basicsPy/LinkList.py
@@ -18,6 +18,28 @@ class LinkList:
                 temp=temp.next
             temp.next=newNode
             print(f"{data} inserted successfully!")
+
+    def deleteLast(self):
+         if self.head is None:
+             print("List is empty Nothing to delete..")
+             return
+         if self.head.next is None:
+             print(f"deleting last element:{self.head.data}")
+             self.head=None
+             return
+         
+         temp=self.head
+         while temp.next.next:
+             temp=temp.next
+         print(f"deleting last element:{temp.next.data}")
+         temp.next=None
+    
+    def deleteList(self):
+        print("List deleted!")
+        self.head=None
+
+
+
     def display(self):
         if self.head is Node:
             print("list is empty!")
@@ -29,8 +51,11 @@ class LinkList:
             print("None")
 
 LL=LinkList()
+
+
+
 while True:
-    print("1.insert data \n2.display \n3.exit")
+    print("1.insert data \n2.display \n3.delete last element \n4.delete List \n5.exit")
     ch=int(input("enter your choice:"))
     if ch==1:
        data=int(input("enter data:"))
@@ -38,6 +63,10 @@ while True:
     elif ch==2:
         LL.display()
     elif ch==3:
+        LL.deleteLast()
+    elif ch==4:
+        LL.deleteList()
+    elif ch==5:
         print("existing program....")
         break
     else:
